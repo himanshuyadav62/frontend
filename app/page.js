e5205ca0cf9a5308108418a50702a3dbaa20ce66
Himanshu yadav <119734736+himanshuyadav62@users.noreply.github.com>
2024-08-02 17:29:20 +0530

    chNGE

--- app/page.js
+++ app/page.js
@@ -5,12 +5,14 @@ import UserList from './components/UserList';
 import ChatBox from './components/ChatBox';
 import SignupForm from './components/SignupForm';
 import './globals.css';
+import { chatPage } from '@/components/component/chat-page';
 
 const Page = () => {
-  const [token, setToken] = useState(null);
+
+  const [token, setToken] = useState('eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyYWh1bC5zaGFybWFAaW5jdHVyZS5jb20iLCJpYXQiOjE3MjI0ODkxOTksImV4cCI6MTcyMjU3NTU5OX0.N_8jdX0ZwVIC8mCL9WwjPkogxoFpetembExhW1bxkgDeYD0hrclHCSyoyYMsbZQ1s8Z61DxTDh8toej4kP_k_Q');
   const [selectedUser, setSelectedUser] = useState(null);
   const [showSignup, setShowSignup] = useState(false);
-
+  console.log(token);
   if (showSignup) {
     return <SignupForm onSignup={() => setShowSignup(false)} />;
   }
